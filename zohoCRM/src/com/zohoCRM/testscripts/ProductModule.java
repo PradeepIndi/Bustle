5d592bd1d6f7177f217c0755e6f28d79bdab435f
pprad <pprad@DESKTOP-AHV403J>
2022-10-14 20:42:09 +0530

    Product Module Committed

---
+++ zohoCRM/src/com/zohoCRM/testscripts/ProductModule.java
@@ -0,0 +1,14 @@
+package com.zohoCRM.testscripts;
+
+import org.testng.annotations.Test;
+
+import com.zohoCRM.generic.BaseClass;
+
+public class ProductModule extends BaseClass{
+	
+	@Test
+	public void createProduct() {
+		
+	}
+
+}
